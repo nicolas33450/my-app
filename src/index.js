22abd1fa63cdd7b978ac4f1c84097e0bd70e65d0
nicolas33450 <40290258+nicolas33450@users.noreply.github.com>
2018-07-25 21:03:57 +0200

    dd

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+//import './index.css';//
 //import App from './App';
-import Devine from './Component/Devine/Devine';
+import TodoList from './Component/TodoList/TodoList';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Devine />, document.getElementById('root'));
+ReactDOM.render(<TodoList />, document.getElementById('root'));
 registerServiceWorker();
